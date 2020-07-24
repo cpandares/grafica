9cd40948669ba5bb8878bbd4eb404257675516a3
unknown <pandaresocesar@gmail.com>
2020-07-23 21:52:08 -0400

    key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,13 +6,13 @@ export const environment = {
   production: false,
   url : 'http://localhost:5000/firestore-grafica-2e08a/us-central1',
   firebase: {
-    apiKey: "AIzaSyBmkFekGm16IVgfv4C0gHYs6ZjawIYkoCY",
-    authDomain: "firestore-grafica-2e08a.firebaseapp.com",
-    databaseURL: "https://firestore-grafica-2e08a.firebaseio.com",
-    projectId: "firestore-grafica-2e08a",
-    storageBucket: "firestore-grafica-2e08a.appspot.com",
-    messagingSenderId: "506332957128",
-    appId: "1:506332957128:web:64c5b3286d00926d2318fd"
+    apiKey: "loremadasdoñasdñioahdoiasdñoia",
+    authDomain: "lashdasldasdioasasd",
+    databaseURL: "flhsdpfhsdpfhsdf",
+    projectId: "oñasdñiasdjoñiasdjasd",
+    storageBucket: "aosidhasiodpdasd",
+    messagingSenderId: "asiodhasiodhasiodhiopasd",
+    appId: "#######################"
   }
 };
 
